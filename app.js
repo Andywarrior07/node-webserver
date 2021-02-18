596c5795a9eca2fd692c3a3d970659ce6a163a21
andresguerrero07 <andres@legalbot.cl>
2021-02-18 11:08:14 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+const port = 8080;
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    name: 'Andy',
+    title: 'Curso de Node',
+  });
+});
+
+app.get('/generic', (req, res) => {
+  res.render('generic', {
+    name: 'Andy',
+    title: 'Curso de Node',
+  });
+});
+
+app.get('/elements', (req, res) => {
+  res.render('elements', {
+    name: 'Andy',
+    title: 'Curso de Node',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
